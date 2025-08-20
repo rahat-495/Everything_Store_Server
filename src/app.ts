7b189524c06fa25056a02dd2f685c899fa300ec2
rahat-495 <md8702687@gmail.com>
2025-08-20 15:35:22 +0600

    router setup for main routes !

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser' ;
+import router from './app/routes';
 
 const app = express() ;
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials : true , origin : "http://localhost:3000"}));
+
+app.use("/api/v1" ,router) ;
 
 app.get('/' , (req , res) => {
     res.send("Everything store server is running !") ;
