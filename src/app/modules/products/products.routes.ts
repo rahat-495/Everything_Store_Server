06c8c06675aa66449b12fcad7386916413860050
rahat-495 <md8702687@gmail.com>
2025-09-07 16:29:39 +0600

    product route and validation schema are updated !

--- src/app/modules/products/products.routes.ts
+++ src/app/modules/products/products.routes.ts
@@ -2,10 +2,12 @@
 import { Router } from "express";
 import { productControllers } from "./products.controllers";
 import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { productValidations } from "./products.validations";
 
 const router = Router() ;
 
 router.get('/' , productControllers.getAllProducts) ;
-router.post('/create-products' , productControllers.createProduct) ;
+router.post('/create-product' , validateRequest(productValidations.createProductValidationSchema) , productControllers.createProduct) ;
 
 export const productRoutes = router ;
--- src/app/modules/products/products.validations.ts
+++ src/app/modules/products/products.validations.ts
@@ -1,2 +1,36 @@
 
+import z from "zod";
 
+const createProductValidationSchema = z.object({
+    body : z.object({
+        description: z.string({
+        required_error: "Description is required",
+        }),
+
+        image: z.string({
+            required_error: "Image URL is required",
+        }),
+
+        discount: z.number().optional(),
+
+        previousPrice: z.number().optional(),
+
+        price: z.number({
+            required_error: "Price is required",
+        }),
+
+        title: z.string({
+            required_error: "Title is required",
+        }),
+
+        inStock: z.boolean().default(true).optional(),
+
+        quantity: z.number({
+            required_error: "Quantity is required",
+        }),
+    })
+});
+
+export const productValidations = {
+  createProductValidationSchema,
+};
