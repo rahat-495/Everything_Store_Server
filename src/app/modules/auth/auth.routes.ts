8548fed5f984738914e2ff35692d21817953c91a
rahat-495 <md8702687@gmail.com>
2025-09-21 22:30:27 +0600

    logout route are create at auth module !

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -6,5 +6,6 @@ const router = Router() ;
 
 router.post("/register" , authControllers.registerUser) ;
 router.post("/login" , authControllers.loginUser) ;
+router.post("/logout" , authControllers.logoutUser) ;
 
 export const authRoutes = router ;
